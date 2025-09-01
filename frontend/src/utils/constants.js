3bdc11d6e91a663277a32fd38919ceb5e2e2143c
Mohit Sharma <140817367+MohitSharma7256@users.noreply.github.com>
2025-09-01 15:53:30 +0000

    backend data correct

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000'
-export const BASE_URL = 'https://triply-backend-4p5t.onrender.com'
+export const BASE_URL = 'gh repo clone MohitSharma7256/Triply'
